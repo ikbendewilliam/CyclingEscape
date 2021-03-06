64544c13a584ed90cc3ccc4a0de423c42569c754
William Verhaeghe <ikbendewilliam@hotmail.com>
2021-03-06 23:08:38 +0100

    Increased build number

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:CyclingEscape/views/gameManager.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flame/util.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   GameManager gameManager = new GameManager();
-  runApp(gameManager.widget);
+  runApp(app(gameManager.widget));
   Util flameUtil = Util();
   await flameUtil.fullScreen();
   await flameUtil.setLandscape();
   gameManager.load();
 }
+
+Widget app(Widget gameWidget) => DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => gameWidget, // Wrap your app
+    );
